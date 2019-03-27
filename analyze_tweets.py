0b28fabdb5afef0751cda34f3fafadb1d796f8ef
dwightcaballero <dwightcaballero1999@gmail.com>
2019-03-27 22:26:08 +0800

    sentiment analysis (en, tl)

--- analyze_tweets.py
+++ analyze_tweets.py
@@ -1,14 +1,20 @@
 from textblob import TextBlob
+from googletrans import Translator
 import json
-import gather_tweets as gt
 
-with open(gt.file_name, 'r') as json_file:
+with open('duterte_drug.json', 'r') as json_file:
     json_data = json.load(json_file)
+    trans = Translator()
 
     for i in range(len(json_data['data'])):
-        text = TextBlob(json_data['data'][i]['tweet_text'])
+        text = json_data['data'][i]['tweet_text']
+        text_lang = trans.detect(text)
 
-        text_lang = text.detect_language()
-        if text_lang != "en":
-            text = text.translate(from_lang=text_lang, to='en')
-        print(text, '\n', text.sentiment, '\n\n')
+        if text_lang.lang != 'en':
+            text_conv = trans.translate(text)
+            text_blob = TextBlob(text_conv.text)
+            print('orig_text: ', text, '\nconv_text: ',
+                  text_conv.text, '\n', text_blob.sentiment, '\n\n')
+        else:
+            text_blob = TextBlob(text)
+            print(text_blob, '\n', text_blob.sentiment, '\n\n')
--- gather_tweets.py
+++ gather_tweets.py
@@ -1,36 +1,14 @@
 import TwitterSearch as ts
 import twitter_credentials as tc
 import json
-from datetime import date
 
 
-def save_tweets(json_data, file_name):
-    with open(file_name, 'w') as json_file:
-        json.dump(json_data, json_file, indent=4, sort_keys=True)
+class modify_tweet:
 
-
-key1 = 'duterte'
-key2 = 'drug'
-
-tso = ts.TwitterSearchOrder()
-tso.set_keywords([key1, key2])
-tso.arguments.update({'tweet_mode': 'extended'})
-
-api = ts.TwitterSearch(
-    consumer_key=tc.consumer_key,
-    consumer_secret=tc.consumer_secret,
-    access_token=tc.access_token,
-    access_token_secret=tc.access_token_secret
-)
-
-json_data = {}
-json_data["data"] = []
-for tweet in api.search_tweets_iterable(tso):
-    if tweet['user']['verified']:
+    def clean_tweet_text(self, tweet):
         try:
             json_temp_text = tweet['retweeted_status']['full_text']
             retweet = True
-
         except KeyError:
             json_temp_text = tweet['full_text']
             retweet = False
@@ -40,17 +18,14 @@ for tweet in api.search_tweets_iterable(tso):
                 for i in range(len(tweet['retweeted_status']['entities']['media'])):
                     json_temp_text = json_temp_text.replace(
                         tweet['retweeted_status']['entities']['media'][i]['url'], '')
-
             if tweet['retweeted_status']['entities']['hashtags']:
                 for i in range(len(tweet['retweeted_status']['entities']['hashtags'])):
                     json_temp_text = json_temp_text.replace(
-                        '#'+tweet['retweeted_status']['entities']['hashtags'][i]['text'], '')
-
+                        '#' + tweet['retweeted_status']['entities']['hashtags'][i]['text'], '')
             if tweet['retweeted_status']['entities']['user_mentions']:
                 for i in range(len(tweet['retweeted_status']['entities']['user_mentions'])):
                     json_temp_text = json_temp_text.replace(
-                        '@'+tweet['retweeted_status']['entities']['user_mentions'][i]['screen_name'], '')
-
+                        '@' + tweet['retweeted_status']['entities']['user_mentions'][i]['screen_name'], '')
             if tweet['retweeted_status']['entities']['urls']:
                 for i in range(len(tweet['retweeted_status']['entities']['urls'])):
                     json_temp_text = json_temp_text.replace(
@@ -61,17 +36,14 @@ for tweet in api.search_tweets_iterable(tso):
                 for i in range(len(tweet['entities']['media'])):
                     json_temp_text = json_temp_text.replace(
                         tweet['entities']['media'][i]['url'], '')
-
             if tweet['entities']['hashtags']:
                 for i in range(len(tweet['entities']['hashtags'])):
                     json_temp_text = json_temp_text.replace(
-                        '#'+tweet['entities']['hashtags'][i]['text'], '')
-
+                        '#' + tweet['entities']['hashtags'][i]['text'], '')
             if tweet['entities']['user_mentions']:
                 for i in range(len(tweet['entities']['user_mentions'])):
                     json_temp_text = json_temp_text.replace(
-                        '@'+tweet['entities']['user_mentions'][i]['screen_name'], '')
-
+                        '@' + tweet['entities']['user_mentions'][i]['screen_name'], '')
             if tweet['entities']['urls']:
                 for i in range(len(tweet['entities']['urls'])):
                     json_temp_text = json_temp_text.replace(tweet['entities']['urls'][i]['url'], '')
@@ -79,17 +51,47 @@ for tweet in api.search_tweets_iterable(tso):
         json_temp_text = json_temp_text.encode('ascii', 'ignore')
         json_temp_text = json_temp_text.decode('utf-8')
         json_temp_text = json_temp_text.replace('\n', "").replace('\"', "").strip()
+        return json_temp_text
 
-        json_data['data'].append({
-            'tweet_text': json_temp_text,
-            'tweet_id': tweet['id'],
-            'tweet_created': tweet['created_at'],
-            'tweet_loc': tweet['coordinates'],
-            'user_id': tweet['user']['id'],
-            'user_created': tweet['user']['created_at'],
-            'user_verified': tweet['user']['verified'],
-            'user_loc': tweet['user']['location']
-        })
+    def save_tweets(self, json_data, file_name):
+        with open(file_name, 'w') as json_file:
+            json.dump(json_data, json_file, indent=4, sort_keys=True)
 
-file_name = key1+'_'+key2+'('+str(date.today())+').json'
-save_tweets(json_data, file_name)
+
+class gather_tweet:
+    mt = modify_tweet()
+
+    key1 = 'duterte'
+    key2 = 'drug'
+
+    tso = ts.TwitterSearchOrder()
+    tso.set_keywords([key1, key2])
+    tso.arguments.update({'tweet_mode': 'extended'})
+
+    api = ts.TwitterSearch(
+        consumer_key=tc.consumer_key,
+        consumer_secret=tc.consumer_secret,
+        access_token=tc.access_token,
+        access_token_secret=tc.access_token_secret
+    )
+
+    json_data = {}
+    json_data["data"] = []
+
+    for tweet in api.search_tweets_iterable(tso):
+        if tweet['user']['verified']:
+            tweet_text = mt.clean_tweet_text(tweet)
+
+            json_data['data'].append({
+                'tweet_text': tweet_text,
+                'tweet_id': tweet['id'],
+                'tweet_created': tweet['created_at'],
+                'tweet_loc': tweet['coordinates'],
+                'user_id': tweet['user']['id'],
+                'user_created': tweet['user']['created_at'],
+                'user_verified': tweet['user']['verified'],
+                'user_loc': tweet['user']['location']
+            })
+
+    file_name = key1 + '_' + key2 + '.json'
+    mt.save_tweets(json_data, file_name)
