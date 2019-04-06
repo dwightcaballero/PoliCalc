745aff7ffeeecedc648467d945b0653f5ed16fe1
Dwight Kenn Caballero <47337213+dwightcaballero@users.noreply.github.com>
2019-04-06 09:54:02 +0800

    deleted twitter credentials for security

--- twitter_credentials.py
+++
@@ -1,5 +0,0 @@
-# Twitter Credentials (Dwight Seu)
-consumer_key = "JwUkEgxaHGamnY8vw6o9HFTlI"
-consumer_secret = "JVlWOsbj1uDTWwCZ6g1ThT95kVGFjLDCgQBoRdHTOEZfCfJxMl"
-access_token = "719916492331098112-2iviJfFYNs49Ur4c5Joo7SG6yfbgUDr"
-access_token_secret = "ljKVR1aq2jYvQMjoa0iTUz0alE4evpVEqgooMWd8G2kNk"
