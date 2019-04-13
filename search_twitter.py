9a387f994637a82a6ccf1d255970740f40e0d378
dwightcaballero <dwightcaballero1999@gmail.com>
2019-04-13 15:47:53 +0800

    changes in searching tweets

--- search_twitter.py
+++ search_twitter.py
@@ -12,10 +12,10 @@ class modify_tweets:
     def clean_tweet(self, tweet):
 
         pr.set_options(pr.OPT.URL, pr.OPT.MENTION, pr.OPT.HASHTAG,
-                       pr.OPT.EMOJI, pr.OPT.SMILEY)
+                       pr.OPT.EMOJI, pr.OPT.SMILEY, pr.OPT.RESERVED)
 
-        tweet = pr.clean(tweet).replace('&amp', "").replace('\"', "").split(
-            '|')[0].strip().encode('ascii', 'ignore').decode('utf-8')
+        tweet = pr.clean(tweet).replace('&amp', "").strip().encode(
+            'ascii', 'ignore').decode('utf-8')
 
         return tweet
 
@@ -63,19 +63,15 @@ class gather_tweets:
                 json_data[senator] = {}
                 print('[S] ' + senator)
 
-                with open('top_concerns.txt', 'r') as concerns:
+                with open('final_concerns.txt', 'r') as concerns:
 
-                    limit = 0
                     for con in concerns:
-                        limit += 1
-                        if limit > 3:
-                            continue
-
                         con_en = con.split(',')[0]
                         con_tl = con.split(', ')[1]
-                        con_cb = con.split(', ')[2].split(':')[0]
+                        con_cb = con.split(', ')[2].split('\n')[0]
                         con_list = [con_en, con_tl, con_cb]
                         print('\t[C] ' + con_en)
+
                         for concern in con_list:
                             json_data[senator][con_en] = []
                             tso.set_keywords([senator, concern])
@@ -85,7 +81,7 @@ class gather_tweets:
                                 loc = json.load(loc_json)
                                 for i in range(len(loc['location'])):
                                     tso.set_geocode(loc['location'][i]['lat'],
-                                                    loc['location'][i]['long'], 15, False)
+                                                    loc['location'][i]['long'], 10, False)
 
                                     for tweet in api.search_tweets_iterable(tso, callback=self.avoid_rate_limit):
                                         if tweet['id_str'] in id_list and tweet['full_text'] in tweet_list:
@@ -130,7 +126,7 @@ class gather_tweets:
                                                 'tweet_id': tweet['id'],
                                                 'rt_count': tweet['retweet_count'],
                                                 'tweet_created': tweet['created_at'],
-                                                'tweet_loc': tweet['coordinates'],
+                                                'tweet_loc': loc['location'][i]['city'],
                                                 'user_id': tweet['user']['id'],
                                                 'user_created': tweet['user']['created_at'],
                                                 'user_verified': tweet['user']['verified'],
@@ -150,7 +146,11 @@ class gather_concerns:
         print('Analyzing most talked national concerns in Twitter...')
         con_total = {}
 
-        with open('concerns.txt', 'r') as concerns:
+        with open('survey_concerns.txt', 'r') as concerns:
+
+            with open('final_concerns.txt', 'w') as final:
+
+                final_concerns = [concerns.readline(), concerns.readline(), concerns.readline()]
 
             for con in concerns:
                 con_en = con.split(',')[0]
@@ -162,8 +162,14 @@ class gather_concerns:
 
             top_list = sorted(con_total.items(), key=lambda kv: kv[1], reverse=True)
 
-            with open('top_concerns.txt', 'w') as top:
+            with open('twitter_concerns.txt', 'w') as top:
+
+                limit = 0
                 for i in range(len(top_list)):
+                    if limit < 3:
+                        if top_list[i][0] not in final_concerns:
+                            final_concerns.append(top_list[i][0])
+                        limit += 1
                     top.write(top_list[i][0] + ': ' + str(top_list[i][1]) + '\n')
 
                 gathered_at = datetime.datetime.now()
@@ -172,6 +178,9 @@ class gather_concerns:
                 gathered_at = gathered_at.strftime("%B %d, %Y | %I:%M %p")
 
                 top.write(week_ago + ' - ' + gathered_at)
+
+                for i in range(len(final_concerns)):
+                    final.write(final_concerns[i] + '\n')
 
         print('Finished gathering the most talked national concerns in Twitter...')
 
@@ -182,6 +191,7 @@ class gather_concerns:
         api = auth_twitter.authenticate().get_api()
         con_count = 0
         respo_list = []
+        respo_loc = []
 
         for con in con_list:
             tso.set_keywords([con])
@@ -189,22 +199,28 @@ class gather_concerns:
             with open('city_coordinates.json') as loc_json:
 
                 loc = json.load(loc_json)
-
                 for i in range(len(loc['location'])):
                     tso.set_geocode(loc['location'][i]['lat'],
-                                    loc['location'][i]['long'], 15, False)
+                                    loc['location'][i]['long'], 10, False)
 
                     for tweet in api.search_tweets_iterable(tso, callback=self.avoid_rate_limit):
-                        cleaned_tweet = modify_tweets().clean_tweet(tweet['full_text'])
-                        temp_res = tweet['id_str'] + ': ' + cleaned_tweet
+                        try:
+                            tweet_text = tweet['retweeted_status']['full_text']
+                        except KeyError:
+                            tweet_text = tweet['full_text']
+
+                        cleaned_tweet = modify_tweets().clean_tweet(tweet_text)
+                        temp_res = cleaned_tweet + ' - ' + tweet['id_str']
                         if temp_res not in respo_list:
                             respo_list.append(temp_res)
+                            respo_loc.append(loc['location'][i]['city'])
                             con_count += 1
 
         with open('response.txt', 'a') as res:
             res.write(con_list[0] + ': ' + str(con_count) + '\n')
-            for i in respo_list:
-                res.write(i + '\n')
+            for i in range(len(respo_list)):
+                response = respo_list[i] + ' (' + respo_loc[i] + ')'
+                res.write(response + '\n')
 
         return con_count
 
